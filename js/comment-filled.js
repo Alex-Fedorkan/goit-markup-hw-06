7fb5358cc34d6c72d40cacba6da4bff015be57dc
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2020-06-11 23:09:10 +0300

    Add custom checkbox. Change animation in modal. Delete outline:none.

--- js/comment-filled.js
+++
@@ -1,8 +0,0 @@
-$('.comment-box__input').on('input', function () {
-  var $this = $(this);
-  if ($this.val() == '') {
-    $this.removeClass('comment-box__input--filled');
-  } else {
-    $this.addClass('comment-box__input--filled');
-  }
-});
--- js/field-filled.js
+++
@@ -1,8 +0,0 @@
-$('.field-box__input').on('input', function () {
-  var $this = $(this);
-  if ($this.val() == '') {
-    $this.removeClass('field-box__input--filled');
-  } else {
-    $this.addClass('field-box__input--filled');
-  }
-});
